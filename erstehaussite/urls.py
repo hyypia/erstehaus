1274a0c7a1b869a7a90db076c9b5054560edab34
igor <igortereschuk92@gmail.com>
2025-06-14 22:39:47 +0200

    feat(home): add home page

--- erstehaussite/urls.py
+++ erstehaussite/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("home.urls")),
     path("news/", include("news.urls")),
     path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
